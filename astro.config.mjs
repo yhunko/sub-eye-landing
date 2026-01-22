4658cf20469c382950f465dc1cfc885d04984678
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-01-22 16:04:51 +0200

    fix: update site URL to include "www"

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://subeye.cc",
+  site: "https://www.subeye.cc",
 
   i18n: {
     locales: ["en", "ua"],
