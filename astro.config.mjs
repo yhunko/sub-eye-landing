d9f2f4628682423dea0eced155d456b7660ab644
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-01-22 20:49:43 +0200

    feat: add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -33,4 +32,12 @@ export default defineConfig({
       },
     }),
   ],
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+    devImageService: "sharp",
+  }),
 });
